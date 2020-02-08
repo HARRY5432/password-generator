be4929a298d01ae15e62f6462a97600e065150ba
HARRY5432 <poter5432@gmail.com>
2020-02-08 10:00:00 +0530

    oops forgot to add __name__ check

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,9 @@
 import random
 import string
 
-length = 12
-chars = string.ascii_letters + string.digits
-password = "".join(random.choice(chars) for _ in range(length))
-print(password)
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits
+    return "".join(random.choice(chars) for _ in range(length))
+
+if __name__ == "__main__":
+    print(generate_password())
