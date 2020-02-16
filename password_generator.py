e441375d8740593f2d1d7573e5ec046253ff0a9b
HARRY5432 <poter5432@gmail.com>
2020-02-16 10:00:00 +0530

    added more symbols to the pool

--- password_generator.py
+++ password_generator.py
@@ -3,9 +3,14 @@ import random
 import string
 import sys
 
+DIGITS = string.digits
+LOWERCASE = string.ascii_lowercase
+UPPERCASE = string.ascii_uppercase
+SYMBOLS = "!@#$%^&*()-_=+[]{};:,.<>?"
+
 def generate_password(length=12):
     """Generate a single random password."""
-    chars = string.ascii_letters + string.digits
+    chars = LOWERCASE + UPPERCASE + DIGITS + SYMBOLS
     return "".join(random.choice(chars) for _ in range(length))
 
 if __name__ == "__main__":
