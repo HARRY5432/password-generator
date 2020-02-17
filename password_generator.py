8da550930a0812420b4b29e3bd08edad4b4c5afc
HARRY5432 <poter5432@gmail.com>
2020-02-17 10:00:00 +0530

    added character class options

--- password_generator.py
+++ password_generator.py
@@ -8,9 +8,17 @@ LOWERCASE = string.ascii_lowercase
 UPPERCASE = string.ascii_uppercase
 SYMBOLS = "!@#$%^&*()-_=+[]{};:,.<>?"
 
-def generate_password(length=12):
+def generate_password(length=12, use_digits=True, use_lower=True, use_upper=True, use_symbols=True):
     """Generate a single random password."""
-    chars = LOWERCASE + UPPERCASE + DIGITS + SYMBOLS
+    chars = ""
+    if use_digits:
+        chars += DIGITS
+    if use_lower:
+        chars += LOWERCASE
+    if use_upper:
+        chars += UPPERCASE
+    if use_symbols:
+        chars += SYMBOLS
     return "".join(random.choice(chars) for _ in range(length))
 
 if __name__ == "__main__":
