55d98a24b18bcd50a4d554b0950c5a94d5a52431
HARRY5432 <poter5432@gmail.com>
2020-02-19 10:00:00 +0530

    added ambiguous character filter

--- password_generator.py
+++ password_generator.py
@@ -7,8 +7,9 @@ DIGITS = string.digits
 LOWERCASE = string.ascii_lowercase
 UPPERCASE = string.ascii_uppercase
 SYMBOLS = "!@#$%^&*()-_=+[]{};:,.<>?"
+AMBIGUOUS = "Il1O0o"
 
-def generate_password(length=12, use_digits=True, use_lower=True, use_upper=True, use_symbols=True):
+def generate_password(length=12, use_digits=True, use_lower=True, use_upper=True, use_symbols=True, no_ambiguous=False):
     """Generate a single random password."""
     chars = ""
     if use_digits:
@@ -19,6 +20,8 @@ def generate_password(length=12, use_digits=True, use_lower=True, use_upper=True
         chars += UPPERCASE
     if use_symbols:
         chars += SYMBOLS
+    if no_ambiguous:
+        chars = "".join(c for c in chars if c not in AMBIGUOUS)
     return "".join(random.choice(chars) for _ in range(length))
 
 if __name__ == "__main__":
